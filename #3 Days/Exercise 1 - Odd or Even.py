7c89c286addcebfecd65b6392c2fcde1b34cadfd
JONATHAN_CARVALHO <jonathan.carvalho@adoro.com.br>
2022-05-10 23:00:19 -0300

    💄 Tree Commit

---
+++ #3 Days/Exercise 1 - Odd or Even.py
@@ -0,0 +1,9 @@
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+result = number % 2
+#Write your code below this line 👇
+if result == 0:
+    print("This is an even number")
+else:
+    print("This is an odd number")
