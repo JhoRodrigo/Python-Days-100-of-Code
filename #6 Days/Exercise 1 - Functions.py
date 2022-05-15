ac4449cceb80891f79c5c5b145a89349a25410c5
JONATHAN_CARVALHO <jonathan.carvalho@adoro.com.br>
2022-05-15 19:32:02 -0300

    ✅ 6 Days

---
+++ #6 Days/Exercise 1 - Functions.py
@@ -0,0 +1,15 @@
+#Teste realizado no Reeborg's World
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+#Draw Square
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()
