980fd81fd939f957cab0171e4c3ed3b7adf125e3
JhoRodrigo <jrodrigomdc@hotmail.com>
2025-04-24 22:58:08 -0300

    #1 Days

--- #1 Days/Projeto.py
+++ #1 Days/Projeto.py
@@ -1,5 +1,5 @@
 #1. Create a greeting for your program.
-print("Welcome to the band name generator.")
+print("Welcome to the band name generator.\n")
 #2. Ask the user for the city that they grew up in.
 city = input("Which city did you grow up in?\n")
 #3. Ask the user for the name of a pet.
